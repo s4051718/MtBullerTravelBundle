98ee11ceb6ab651c9be3167729f75214e90ea9dc
Brayden Franklin <s4051718>
2026-04-18 18:54:21 +1000

    feat(app): implement core resort management logic and ux pause mechanism

--- src/MtBullerResort.java
+++ src/MtBullerResort.java
@@ -55,42 +55,246 @@ public class MtBullerResort {
     }
 
     public void displayAllAccommodations() {
+        System.out.println("\n--------------------------------");
+        System.out.println("All Accommodations");
 
+        for (Accommodation room : accommodations) {
+            System.out.println(room);
+        }
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void displayAvailableAccommodations() {
+        System.out.println("\n--------------------------------");
+        System.out.println("Available Accommodations");
 
+        for (Accommodation room : accommodations) {
+            if (room.isAvailable()) {
+                System.out.println(room);
+            }
+        }
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void addCustomer() {
+        System.out.println("\n--------------------------------");
+        System.out.println("Create New Customer Account");
+        System.out.println("--------------------------------");
 
+        System.out.println("Please enter an ID for the customer: ");
+        int newId = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("\nPlease enter the customers name: ");
+        String newName = scanner.nextLine();
+
+        System.out.println("\nPlease enter the customers skiing level: "
+        + "\n1). Beginner"
+        + "\n2). Intermediate"
+        + "\n3). Expert"
+        );
+        int levelChoice = scanner.nextInt();
+        scanner.nextLine();
+        SkiingLevel skiLevel = null;
+
+        switch (levelChoice) {
+            case 1:
+                skiLevel = SkiingLevel.BEGINNER;
+                break;
+            case 2:
+                skiLevel = SkiingLevel.INTERMEDIATE;
+                break;
+            case 3:
+                skiLevel = SkiingLevel.EXPERT;
+                break;
+            default:
+                break;
+        }
+
+        Customer customer = new Customer(newId, newName, skiLevel);
+
+        customers.add(customer);
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void listCustomers() {
+        System.out.println("\n--------------------------------");
+        System.out.println("List of Customers");
 
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void createTravelBundle() {
+        System.out.println("\n--------------------------------");
+        System.out.println("Create Travel Bundle");
+        System.out.println("--------------------------------");
 
+        // Customer ID
+        Customer selectedCustomer = null;
+
+        System.out.println("Please enter the customers ID number: ");
+        int targetCustomerId = scanner.nextInt();
+        scanner.nextLine();
+
+        for (Customer currentCustomer : customers) {
+            if (currentCustomer.getId() == targetCustomerId) {
+                selectedCustomer = currentCustomer;
+            }
+        }
+
+        // Accommodation ID
+        Accommodation selectedAccommodation = null;
+
+        System.out.println("\nPlease enter the Accommodation ID: ");
+        String targetAccommodationId = scanner.nextLine();
+
+        for (Accommodation currentAccommodation : accommodations) {
+            if (currentAccommodation.getId().equals(targetAccommodationId)) {
+                selectedAccommodation = currentAccommodation;
+            }
+        }
+
+        System.out.println("\nHow many nights accommodation?: ");
+        int accommodationNights = scanner.nextInt();
+        scanner.nextLine();
+
+        // Lift Pass
+        System.out.println("\nHow many days lift access?: ");
+        int passDays = scanner.nextInt();
+        scanner.nextLine();
+
+        LiftPassType passType = null;
+
+        if (passDays == 0) {
+            passType = LiftPassType.NONE;
+        } else if (passDays >= 365) {
+            passType = LiftPassType.SEASON;
+        } else if (passDays >= 5) {
+            passType = LiftPassType.FIVE_DAYS;
+        } else {
+            passType = LiftPassType.SINGLE_DAY;
+        }
+
+        // Lessons
+        System.out.println("\nHow many lessons? "); //fix wording
+        int lessons = scanner.nextInt();
+        scanner.nextLine();
+
+        // Bundle Creation
+        TravelBundle newBundle = new TravelBundle(selectedCustomer, selectedAccommodation, passType, accommodationNights, passDays, lessons);
+
+        travelBundles.add(newBundle);
+        selectedAccommodation.setAvailable(false);
+
+        // Display Bundle
+        System.out.println("\n" + newBundle);
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void listTravelBundles() {
+        System.out.println("\n--------------------------------");
+        System.out.println("List of Travel Bundles");
 
+        for (TravelBundle bundle : travelBundles) {
+            System.out.println(bundle);
+        }
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void addLiftPassToBundle() {
+        System.out.println("\n--------------------------------");
+        System.out.println("Add Lift Pass To Bundle");
+        System.out.println("--------------------------------");
 
+        System.out.println("Please enter the customers ID number: ");
+        int targetCustomerId = scanner.nextInt();
+        scanner.nextLine();
+
+        TravelBundle selectedTravelBundle = null;
+
+        for (TravelBundle bundle : travelBundles) {
+            if (bundle.getCustomer().getId() == targetCustomerId) {
+                selectedTravelBundle = bundle;
+                break;
+            }
+        }
+
+        System.out.println("\nHow many days lift access?: ");
+        int passDays = scanner.nextInt();
+        scanner.nextLine();
+
+        LiftPassType passType = null;
+
+        if (passDays == 0) {
+            passType = LiftPassType.NONE;
+        } else if (passDays >= 365) {
+            passType = LiftPassType.SEASON;
+        } else if (passDays >= 5) {
+            passType = LiftPassType.FIVE_DAYS;
+        } else {
+            passType = LiftPassType.SINGLE_DAY;
+        }
+
+        selectedTravelBundle.setLiftPass(passType);
+        selectedTravelBundle.setLiftPassDays(passDays);
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void addLessonToBundle() {
+        System.out.println("\n--------------------------------");
+        System.out.println("Add Lesson To Bundle");
+        System.out.println("--------------------------------");
 
+       System.out.println("Please enter the customers ID number: ");
+        int targetCustomerId = scanner.nextInt();
+        scanner.nextLine();
+
+        TravelBundle selectedTravelBundle = null;
+
+        for (TravelBundle bundle : travelBundles) {
+            if (bundle.getCustomer().getId() == targetCustomerId) {
+                selectedTravelBundle = bundle;
+                break;
+            }
+        }
+
+        int lessons;
+
+        System.out.println("How many lessons? ");
+        lessons = scanner.nextInt();
+        scanner.nextLine();
+
+        selectedTravelBundle.setNumberofLessons(selectedTravelBundle.getNumberofLessons() + lessons);
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void writeBundleToFile() {
-
+        System.out.println("\n--------------------------------");
+        System.out.println("Write Bundle To File");
+        System.out.println("--------------------------------");
+        pauseForUser();
     }
 
     public void readBundleFromFile() {
+        System.out.println("\n--------------------------------");
+        System.out.println("Read Bundle From File");
+        System.out.println("--------------------------------");
+        pauseForUser();
+    }
 
+    private void pauseForUser() {
+        System.out.println("\nPress Enter to return to the main menu...");
+        scanner.nextLine();
     }
 }
